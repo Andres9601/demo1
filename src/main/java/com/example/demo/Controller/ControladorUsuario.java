08581907a2470173ddc5ed73d8e50c7cba146080
Andres9601 <sandresi9601@gmail.com>
2022-12-12 12:39:58 -0500

    UsuarioRepository

---
+++ src/main/java/com/example/demo/Controller/ControladorUsuario.java
@@ -0,0 +1,40 @@
+package com.example.demo.Controller;
+
+
+import java.net.URISyntaxException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.Repository.ManejadorUsuario;
+
+
+@RestController
+public class ControladorUsuario {
+	
+	
+	@Autowired
+	ManejadorUsuario manejadorUsuario;
+
+	
+	@GetMapping(path= "/consultar", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<?> consultar() throws URISyntaxException{
+		
+		try { 
+			return ResponseEntity.ok(manejadorUsuario.findAll());
+			
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("No se puede responder a tu solicitud en este momento "+e);
+		}
+		
+	}
+	
+	
+	@GetMapping(path="/mensaje")
+	public String mensaje() {
+		return "Hola Azure";
+	}
+}
---
+++ src/main/java/com/example/demo/Repository/ManejadorUsuario.java
@@ -0,0 +1,14 @@
+package com.example.demo.Repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.Entity.Usuario;
+
+
+@Repository
+public interface ManejadorUsuario extends JpaRepository<Usuario, String> {
+	
+}
