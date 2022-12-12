0a78f93dd913e88889438760f736576cd9d44da5
Andres9601 <sandresi9601@gmail.com>
2022-12-12 13:01:24 -0500

    ComponentScan

--- src/main/java/com/example/demo/Demo1Application.java
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -2,11 +2,13 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@ComponentScan(basePackages = {"com.example.demo.Controller"})
 public class Demo1Application {
 
 	@GetMapping
