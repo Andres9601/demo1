d5c129ea087f91289c19af1be8caecc714d24c50
Andres9601 <sandresi9601@gmail.com>
2022-12-14 10:38:29 -0500

    controller

--- src/main/java/com/project/ChelpaTex/Controller/ControladorUsuario.java
+++ src/main/java/com/project/ChelpaTex/Controller/ControladorUsuario.java
@@ -7,12 +7,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.project.ChelpaTex.Service.ServicioUsuario;
 
 
 @RestController
+@RequestMapping("/webresources/controller/controladorUsuario")
 public class ControladorUsuario {
 
 	@Autowired(required = true)
