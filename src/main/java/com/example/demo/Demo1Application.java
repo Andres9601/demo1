a5a479ad1bb0bdc92715f7c7bb54524039e14f25
Andres9601 <sandresi9601@gmail.com>
2023-01-27 17:32:04 -0500

    Demo

--- src/main/java/com/example/demo/Demo1Application.java
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import org.apache.tomcat.util.file.ConfigurationSource.Resource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -8,6 +10,8 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan(basePackages = {"com.example.demo.Controller","com.example.demo.Service","com.example.demo.ServiceImpl","com.example.demo.Security"})
 public class Demo1Application {
 
+	@Value("classpath:firebase.json")
+	private Resource configFile;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Demo1Application.class, args);
