5771a57247a47901950f7b2b9f3328c3e1f5d1ae
Andres9601 <sandresi9601@gmail.com>
2023-02-20 00:13:25 -0500

    sihcoasjcoijas

--- src/main/java/com/example/demo/Security/FirebaseInit.java
+++ src/main/java/com/example/demo/Security/FirebaseInit.java
@@ -1,15 +1,11 @@
 package com.example.demo.Security;
 
-import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
 import java.util.logging.Logger;
 
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StreamUtils;
 
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
@@ -19,46 +15,32 @@ import com.google.firebase.auth.FirebaseAuthException;
 @Service
 public class FirebaseInit {
 
-	
-    private static FirebaseOptions firebaseOptions;
-	//private static FileInputStream serviceAccount; 
-	//private static final String FIREBASE_CONFIG = "./src/main/resources/firebase.json";
-	//private static final String FIREBASE_CONFIG = System.getenv("FIREBASE_JSON");
-	
-
-	
-
-	
 	private static final Logger logger =  Logger.getLogger(FirebaseInit.class.getName());
 
+	
+    private static FirebaseOptions firebaseOptions;
+	private static FileInputStream serviceAccount; 
+	//private static final String FIREBASE_CONFIG = "./src/main/resources/firebase.json";
+	//private static final String FIREBASE_CONFIG = System.getenv("FIREBASE_JSON");
 
-	public void inicializacion() throws FirebaseAuthException, IOException {
+
+
+	//public static void inicializacion() throws FirebaseAuthException {
 		
-		// Cargar archivo firebase.json en FirebaseOptions
-		ClassPathResource resource = new ClassPathResource("firebase.json");
-		InputStream inputStream = resource.getInputStream();
-		String json = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
-		firebaseOptions = new FirebaseOptions.Builder()
-		    .setCredentials(GoogleCredentials.fromStream(new ByteArrayInputStream(json.getBytes())))
-		    .build();
-		FirebaseApp.initializeApp(firebaseOptions);
-		
-		
-		/*try {
-			serviceAccount = new FileInputStream(FIREBASE_CONFIG);
-			firebaseOptions = new FirebaseOptions.Builder()
-			.setCredentials(GoogleCredentials.fromStream(serviceAccount))
-			.build();
-			FirebaseApp.initializeApp(firebaseOptions);
-			logger.info("inicializado");
-		} catch (Exception e){
-			logger.info("no inicializado");
-		}*/
-		
-		}
 	
-	
-	
+	private static final String FIREBASE_CONFIG = "firebase.json";
+	public static void inicializacion() throws FirebaseAuthException {
+	    try {
+	        InputStream serviceAccount = FirebaseInit.class.getClassLoader().getResourceAsStream(FIREBASE_CONFIG);
+	        firebaseOptions = new FirebaseOptions.Builder()
+	                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+	                .build();
+	        FirebaseApp.initializeApp(firebaseOptions);
+	        logger.info("inicializado");
+	    } catch (Exception e) {
+	    }
+	}
+
 
 }
 
